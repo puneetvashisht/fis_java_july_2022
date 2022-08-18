6ede82a13352ad7147b85b23db21063cd7b3d008
Puneet Vashisht <puneetvashisht@gmail.com>
2022-08-18 12:31:45 +0530

    course with appropriate responses

--- springboot-jaxrs/springboot-jaxrs/src/main/java/com/fis/springbootjaxrs/CourseRepository.java
+++ springboot-jaxrs/springboot-jaxrs/src/main/java/com/fis/springbootjaxrs/CourseRepository.java
@@ -3,5 +3,7 @@ package com.fis.springbootjaxrs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Integer>{
+	
+	public Course findByTitle(String title);
 
 }
--- springboot-jaxrs/springboot-jaxrs/src/main/java/com/fis/springbootjaxrs/CourseResource.java
+++ springboot-jaxrs/springboot-jaxrs/src/main/java/com/fis/springbootjaxrs/CourseResource.java
@@ -1,16 +1,24 @@
 package com.fis.springbootjaxrs;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+
 
 @Path("/courses")
 @Produces({MediaType.APPLICATION_JSON})
@@ -22,18 +30,59 @@ public class CourseResource {
 	
 	@GET
 	public List<Course> getCourses() {
-//		List<Course> courses = new ArrayList<>();
-//		courses.add(new Course(23, "Angular", 5000));
-//		courses.add(new Course(13, "React", 5000));
-		
 		return courseRepository.findAll();
-//		return courses;
+	}
+	
+	@Path("{id}")
+	@GET
+	public Response getCourse(@PathParam("id") int id) {
+		Optional<Course> courseFound = courseRepository.findById(id);
+		if(courseFound.isPresent()) {
+			Course course =  courseFound.get();
+			return Response.ok(course).build();
+		}
+		return Response.status(Status.NOT_FOUND).build();
+	}
+	
+	@Path("{id}")
+	@DELETE
+	public Response deleteCourse(@PathParam("id") int id) {
+		try {
+			courseRepository.deleteById(id);
+			return Response.status(Status.NO_CONTENT).build();
+		}
+		catch(EmptyResultDataAccessException e) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		
+	}
+	
+	@Path("/by")
+	@GET
+	public Response getCourseByName(@QueryParam("title") String title) {
+		System.out.println(title);
+		Course course = courseRepository.findByTitle(title);
+		if(course == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		return Response.ok(course).build();
 	}
 	
 	@POST
-	public void addCourse(Course course) {
+	public Response addCourse(Course course) {
 		System.out.println(course);
-		courseRepository.save(course);
+		// check if title exists
+		Course foundCourse = courseRepository.findByTitle(course.getTitle());
+		if(foundCourse == null) {
+			courseRepository.save(course);
+			return Response.status(Status.CREATED).build();
+		}
+		return Response.status(Status.CONFLICT).build();
+		
+	}
+	@PUT
+	public void updateCourse(Course newCourse) {
+		courseRepository.save(newCourse);
 	}
 
 }
